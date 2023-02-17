1b3ca9c2cd5537d92b9d22bd3416332232a014ca
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-02-17 01:12:23 +0000

    Print Task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print a line of code using puts
+ * and the code should be followed by a new line
+ * Return: 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

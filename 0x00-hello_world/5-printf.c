cfa25c8a951e3e55d495480ace5df06c7ff9f618
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-02-17 01:29:55 +0000

    printf Task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - C program to print a line using printf
+ * and the output should be followed by a new line
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

8c6b0d2801c376b050057673456cfbbfbe8a404a
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-02-21 23:15:24 +0000

    putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - C program that assign a random number to the
+ * variable n each time it is executed and prints the
+ * last digit of the number stored in the variable n
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char char = '_putchar';
+
+	putchar(char);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project contains C - Functions and nested loops

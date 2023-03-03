53d2d1cda2390827afcf415392b9f44ebd177d43
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-03-03 01:51:21 +0000

    Print a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,10 +3,38 @@
 
 /**
  * print_buffer - a C function that prints a buffer.
+ * print_line - prints bytes of a buffer
  * @b: buffer to be printed
- * @size: bytes of the buffer
+ * @size: size of the buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
  * Return: 0
  */
+
+
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
+}
+
 
 void print_buffer(char *b, int size)
 {

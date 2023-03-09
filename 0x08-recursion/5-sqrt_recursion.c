407702758fc3278380dcb14c69524712f7d15f39
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-03-09 00:36:28 +0000

    Sqaure root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * checksquare - a C function that checks for square root of a number
+ * @x: number to be evaluated
+ * @y: number to be evaluated
+ *
+ * Return: int
+ */
+
+int checksquare(int x, int y)
+{
+	if (x * x == y)
+		return (x);
+	if (x * x > y)
+		return (-1);
+	return (checksquare(x + 1, y));
+}
+
+/**
+ * _sqrt_recursion - a C function that returns the
+ * natural square root of a number.
+ * @n: number to be evaluated
+ *
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (check(1, n));
+}

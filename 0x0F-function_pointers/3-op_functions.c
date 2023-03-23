6c0ddb5876606c50c6f5f5b7a09a36b6ced5f60e
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-03-23 03:36:54 +0000

    Simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add - operator add
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - operator subbstraction
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0
+ */
+
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - operator multiplication
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0
+ */
+
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - operator division
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+ * op_mod - operator mod
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}

a5846602a6081ed46ebe3fade42804e9afbaff0f
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-03-31 00:25:41 +0000

    Prints elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,5 @@
-i#include "lists.h"
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include "lists.h"
 
 /**
  * print_list - a C function that prints all the elements of a list_t list.

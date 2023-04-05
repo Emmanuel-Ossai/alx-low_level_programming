09e31bb8f4308460572fa838977d104aa21c6794
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-04-05 03:45:58 +0000

    Sum of all data in list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,22 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * free_listint - a C function that frees a listint_t list.
+ * sum_listint - a C function that returns the sum of all the
+ * data (n) of a listint_t linked list.
  * @head: Pointer to the head node of the linked list
  *
- * Return: frees all elements of a list
+ * Return: the sum of all the data (n) of a list
  */
 
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
 
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}

824341e187cd0ec5abeb30f8fe5c680224ff1f6f
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-04-05 23:42:28 +0000

    Reverses a list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,19 +9,16 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *previous = NULL;
-	listint_t *current = *head;
+	listint_t *prev = NULL, *current = *head, *next = NULL;
 
 	while (current != NULL)
 	{
-		listint_t *next = current->next;
-
-		current->next = previous;
-		previous = current;
+		next = current->next;
+		current->next = prev;
+		prev = current;
 		current = next;
 	}
 
-	*head = previous;
-
-	return (*head);
+	*head = prev;
+	return (prev);
 }

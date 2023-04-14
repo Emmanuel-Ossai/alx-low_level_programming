50932281637faa16bcb04916fe737072c5de4798
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-04-14 23:52:41 +0000

    ELF Header Update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -9,8 +9,8 @@
 
 bool is_elf(const unsigned char *e_ident);
 void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
+void check_magic(unsigned char *e_ident);
+void check_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
 void print_abi(unsigned char *e_ident);
@@ -53,13 +53,13 @@ void check_elf(unsigned char *e_ident)
 
 
 /**
- * print_magic - a C function that prints magic numbers of an ELF header
+ * check_magic - a C function that prints magic numbers of an ELF header
  * @e_ident: pointer to array containing ELF magic numbers
  *
  * Return: magic number
  **/
 
-void print_magic(unsigned char *e_ident)
+void check_magic(unsigned char *e_ident)
 {
 	int index = 0;
 
@@ -77,13 +77,13 @@ void print_magic(unsigned char *e_ident)
 
 
 /**
- * print_class - a C function that prints class of ELF header
+ * check_class - a C function that prints class of ELF header
  * @e_ident: pointer to array containing ELF class
  *
  * Return: class
  **/
 
-void print_class(unsigned char *e_ident)
+void check_class(unsigned char *e_ident)
 {
 	const char *class_str;
 	unsigned char class = e_ident[EI_CLASS];
@@ -233,35 +233,44 @@ void print_abi_ver(unsigned char *e_ident)
 
 
 /**
- * print_type - Prints the type of an ELF header.
- * @e_type: The ELF type.
- * @e_ident: A pointer to an array containing the ELF class.
- */
-void print_type(unsigned int e_type, unsigned char *e_ident)
+ * print_elf_type - a C function that print ELF header type
+ * @e_type: elf type
+ * @e_ident: pointer to array of ELF class
+ *
+ * Return: Type
+ **/
+
+void print_elf_type(unsigned int e_type, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
+	{
 		e_type >>= 8;
+	}
 
 	printf("  Type:                              ");
 
-	switch (e_type)
+	if (e_type == ET_NONE)
 	{
-	case ET_NONE:
 		printf("NONE (None)\n");
-		break;
-	case ET_REL:
+	}
+	else if (e_type == ET_REL)
+	{
 		printf("REL (Relocatable file)\n");
-		break;
-	case ET_EXEC:
+	}
+	else if (e_type == ET_EXEC)
+	{
 		printf("EXEC (Executable file)\n");
-		break;
-	case ET_DYN:
+	}
+	else if (e_type == ET_DYN)
+	{
 		printf("DYN (Shared object file)\n");
-		break;
-	case ET_CORE:
+	}
+	else if (e_type == ET_CORE)
+	{
 		printf("CORE (Core file)\n");
-		break;
-	default:
+	}
+	else
+	{
 		printf("<unknown: %x>\n", e_type);
 	}
 }
@@ -347,13 +356,13 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
-	print_magic(header->e_ident);
-	print_class(header->e_ident);
+	check_magic(header->e_ident);
+	check_class(header->e_ident);
 	print_data(header->e_ident);
 	print_version(header->e_ident);
 	print_osabi(header->e_ident);
 	print_abi_ver(header->e_ident);
-	print_type(header->e_type, header->e_ident);
+	print_elf_type(header->e_type, header->e_ident);
 	print_entry(header->e_entry, header->e_ident);
 
 	free(header);

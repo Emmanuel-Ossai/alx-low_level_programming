f44d55c1a19cd9feac3a04f33e3443547fbe38dd
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-04-14 22:15:36 +0000

    ELF Header Update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -53,25 +53,25 @@ void check_elf(unsigned char *e_ident)
 
 
 /**
- * print_magic - Prints the magic numbers of an ELF header.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
+ * print_magic - a C function that prints magic numbers of an ELF header
+ * @e_ident: pointer to array containing ELF magic numbers
  *
- * Description: Magic numbers are separated by spaces.
- */
+ * Return: magic number
+ **/
+
 void print_magic(unsigned char *e_ident)
 {
-	int index;
+	int index = 0;
 
 	printf("  Magic:   ");
-
-	for (index = 0; index < EI_NIDENT; index++)
+	while (index < EI_NIDENT)
 	{
 		printf("%02x", e_ident[index]);
-
 		if (index == EI_NIDENT - 1)
 			printf("\n");
 		else
 			printf(" ");
+		index++;
 	}
 }
 

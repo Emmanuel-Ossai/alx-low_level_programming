e67b386be55a53e0229a16465c3fc9991cfc4307
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-04-15 00:03:52 +0000

    ELF Header Update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -16,8 +16,8 @@ void print_version(unsigned char *e_ident);
 void print_abi(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
+void print_entry_point(unsigned long int e_entry, unsigned char *e_ident);
+void elf_close(int elf);
 
 /**
 *is_elf - a C function that checks if a file is an ELF file.
@@ -276,36 +276,35 @@ void print_elf_type(unsigned int e_type, unsigned char *e_ident)
 }
 
 /**
- * print_entry - Prints the entry point of an ELF header.
- * @e_entry: The address of the ELF entry point.
- * @e_ident: A pointer to an array containing the ELF class.
- */
-void print_entry(unsigned long int e_entry, unsigned char *e_ident)
+ * print_entry_point - a C function that prints entry point of ELF header
+ * @e_entry: address of elf entry point
+ * @e_ident: pointer to array of elf class
+ *
+ * Return: entry point
+ **/
+
+void print_entry_point(unsigned long int e_entry, unsigned char *e_ident)
 {
-	printf("  Entry point address:               ");
+	unsigned long int new_e_entry = e_entry;
 
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
-		e_entry = ((e_entry << 8) & 0xFF00FF00) |
-			  ((e_entry >> 8) & 0xFF00FF);
-		e_entry = (e_entry << 16) | (e_entry >> 16);
+		new_e_entry = ((new_e_entry << 8) & 0xFF00FF00) |
+			((new_e_entry >> 8) & 0xFF00FF);
+		new_e_entry = (new_e_entry << 16) | (new_e_entry >> 16);
 	}
 
-	if (e_ident[EI_CLASS] == ELFCLASS32)
-		printf("%#x\n", (unsigned int)e_entry);
-
-	else
-		printf("%#lx\n", e_entry);
+	printf("Entry point address: %#lx\n", new_e_entry);
 }
 
 
 /**
- * close_elf - Closes an ELF file.
+ * elf_close - Closes an ELF file.
  * @elf: The file descriptor of the ELF file.
  *
  * Description: If the file cannot be closed - exit code 98.
  */
-void close_elf(int elf)
+void elf_close(int elf)
 {
 	if (close(elf) == -1)
 	{
@@ -341,7 +340,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (header == NULL)
 	{
-		close_elf(o);
+		elf_close(o);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
@@ -349,7 +348,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (r == -1)
 	{
 		free(header);
-		close_elf(o);
+		elf_close(o);
 		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 		exit(98);
 	}
@@ -363,10 +362,9 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_osabi(header->e_ident);
 	print_abi_ver(header->e_ident);
 	print_elf_type(header->e_type, header->e_ident);
-	print_entry(header->e_entry, header->e_ident);
-
+	print_entry_point(header->e_entry, header->e_ident);
 	free(header);
-	close_elf(o);
+	elf_close(o);
 	return (0);
 }
 

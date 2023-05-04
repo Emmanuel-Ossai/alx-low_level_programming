a088e3d7ed114a0dae624b328fe45ddd923c22dd
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-05-04 20:21:58 +0000

    Checks Endianness

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - a C function that writes the character c to a
+ * Standard Output stream - stdout
+ * @c: The character to be written
+ *
+ * Return: On success 1.
+ * On error, -1 is return
+ **/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
